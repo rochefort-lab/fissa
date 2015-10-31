5e76bd1afd1eaeb5e0df650b5fc3f8c0a538f128
Scott Lowe <scottlowe0@gmail.com>
2015-10-31 11:17:58 +0000

    Add URL to setup.py
    Point to github for the moment

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author = "Sander Keemink & Scott Lowe & Nathalie Rochefort",
     author_email = "swkeemink@scimail.eu",
     description = "A Python Library estimating somatic signals in 2-photon data",
-    url = "NA",
+    url = "https://github.com/rochefort-lab/fissa",
     download_url = "NA",
     package_dir = {NAME: "./fissa"},
     packages=[NAME],
