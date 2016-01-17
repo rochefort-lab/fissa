4a07fb1c84800d15a2283e406ac6f39357db80a4
Scott Lowe <scott.code.lowe@gmail.com>
2016-01-17 21:15:40 +0000

    RF-TST: Parametrize get_frame_number tests
    Had to move away from the unittest.TestCase framework in order
    to do the parametrization.

--- fissa/tests/test_readtiffs.py
+++ fissa/tests/test_readtiffs.py
@@ -4,61 +4,60 @@ Tests for readtiffs.py
 
 import os.path
 import csv
+import pytest
+
+import numpy as np
 from PIL import Image
 
-from .base_test import BaseTestCase
-
+import base_test
 from .. import readtiffs
 
 
-class TestReadTiffs(BaseTestCase):
+# Get a path to the resources folder from the test directory
+RESOURCE_DIRECTORY = os.path.join(base_test.TEST_DIRECTORY, 'resources')
 
-    @classmethod
-    def setUpClass(self):
-        '''
-        Setup the test class
+def get_uniform_resources():
+    '''
+    Fixture
 
-        Get the test database from `tiff_num_frames.csv` and store it
-        as a dictionary in `self.resource_num_frames`. This should have two fields,
-        the first is `filename`, containing the filename of the test
-        image, and the second is `num_frames`, containing the number
-        of images in the tiff stack.
+    Get the test database from `tiff_num_frames.csv` and store it
+    as a dictionary in `self.resource_num_frames`. This should have
+    two fields, the first is `filename`, containing the filename of
+    the test image, and the second is `num_frames`, containing the
+    number of images in the tiff stack.
 
-        The images should be as follows:
-            resources/orange_3x3x1.tif
-                A single tiff with a single pixel coloured (255,128,0)
-                and created with the command:
-                convert -depth 8 -size 3x3 xc:#ff8800 orange_3x3x1.tif
-            resources/black_1x2x3.tif
-                3 frames of black with width 1px and height 2px.
-                convert -depth 8 -size 1x2 xc:#000 xc:#000 xc:#000 \
-                    black_1x2x3.tif
-            resources/white_1x2x3.tif
-                3 frames of white with width 1px and height 2px.
-                convert -depth 8 -size 1x2 xc:#fff xc:#fff xc:#fff \
-                    white_1x2x3.tif
-            resources/grey_1x1x5.tif
-                A tiff stack (multipage tiff) containing 5 images with
-                increasing shades of grey.
-                convert -depth 8 -size 1x1 xc:#808080 xc:#828282 \
-                    xc:#848484 xc:#868686 xc:#888888 grey_1x1x5.tif
-        '''
-        self.resource_directory = os.path.join(self.test_directory,
-                                               'resources')
-        datafile = os.path.join(self.resource_directory, 'uniform_tiffs.csv')
-        self.resource_num_frames = list(csv.DictReader(open(datafile),
-                                                       delimiter=';'))
+    The images should be as follows:
+    resources/lgrey_4x5x1.tif
+        A single tiff (no stack), created with the command:
+        convert -depth 8 -size 4x5 xc:#aaa lgrey_4x5x1.tif
+    resources/black_1x2x3.tif
+        3 frames of black with width 1px and height 2px.
+        convert -depth 8 -size 1x2 xc:#000 xc:#000 xc:#000 \
+            black_1x2x3.tif
+    resources/white_1x2x3.tif
+        3 frames of white with width 1px and height 2px.
+        convert -depth 8 -size 1x2 xc:#fff xc:#fff xc:#fff \
+            white_1x2x3.tif
+    resources/grey_1x1x5.tif
+        A tiff stack (multipage tiff) containing 5 images with
+        increasing shades of grey.
+        convert -depth 8 -size 1x1 xc:#808080 xc:#828282 \
+            xc:#848484 xc:#868686 xc:#888888 grey_1x1x5.tif
+    '''
+    uniform_datafile = os.path.join(RESOURCE_DIRECTORY, 'uniform_tiffs.csv')
+    resources = csv.DictReader(open(uniform_datafile), delimiter=';')
+    return resources
 
-    def test_get_frame_number(self):
-        '''
-        Tests the function get_frame_number
 
-        Use the small set of test resource TIFF files to confirm the
-        number of frames is as expected.
-        '''
-        for row in self.resource_num_frames:
-            expected = float(row['num_frames'])
-            fname = os.path.join(self.resource_directory, row['filename'])
-            img = Image.open(fname)
-            actual = readtiffs.get_frame_number(img)
-            self.assertEqual(actual, expected)
+@pytest.mark.parametrize('row', get_uniform_resources())
+def test_frame_number(row):
+    '''
+    Tests the function get_frame_number
+
+    Use the small set of test resource TIFF files to confirm the
+    number of frames is as expected.
+    '''
+    expected = float(row['num_frames'])
+    img = Image.open(os.path.join(RESOURCE_DIRECTORY, row['filename']))
+    actual = readtiffs.get_frame_number(img)
+    base_test.assert_equal(actual, expected)
