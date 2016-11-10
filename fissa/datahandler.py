601d2030c0541d4291cfa722b1947f2d56080d5e
swk <swkeemink@scimail.eu>
2016-11-10 16:43:29 +0000

    ENH: added data handling script + tests

---
+++ fissa/datahandler.py
@@ -0,0 +1,66 @@
+'''
+FISSA functions to handle image and roi objects and return the right
+format.
+
+Authors:
+    Sander W Keemink <swkeemink@scimail.eu>
+    Scott C Lowe <scott.code.lowe@gmail.com>
+
+'''
+
+import numpy as np
+import tifffile
+import roitools
+
+
+def image2array(image):
+    ''' Takes the object 'image' and returns an array.
+
+    Parameters
+    ---------
+    image : unknown
+        The data. Should be either a tif location, or a list
+        of already loaded in data.
+
+    Returns
+    -------
+    np.array
+        A 3D array containing the data as (frames, y coordinate, x coordinate)
+    '''
+    if type(image) == str:
+        return tifffile.imread(image)
+
+    if type(image) == np.array:
+        return image
+
+
+def rois2masks(rois, shape):
+    ''' Takes the object 'rois' and returns it as a list of binary masks.
+
+    Parameters
+    ----------
+    rois : unkown
+        Either a string with imagej roi zip location, list of arrays encoding
+        polygons, or binary arrays representing masks.
+    shape : tuple
+        Shape of the original data in x and y coordinates (x,y)
+    Returns
+    -------
+    list
+        List of binary arrays (i.e. masks)
+
+    '''
+    # if it's a list of strings
+    if type(rois) == str:
+        rois = roitools.readrois(rois)
+    if type(rois) == list:
+        # if it's a something by 2 array, assume polygons
+        if rois[0].shape[1] == 2:
+            return roitools.getmasks(rois, shape)
+
+        # if it's a list bigger arrays, assume masks
+        elif rois[0].shape == shape:
+            return rois
+
+    else:
+        raise ValueError('Wrong rois input format')
---
+++ fissa/tests/test_datahandler.py
@@ -0,0 +1,67 @@
+'''
+Tests for datahandler.py
+'''
+
+import numpy as np
+from PIL import Image
+import tifffile
+import os
+
+import base_test
+from .. import datahandler
+from .. import roitools
+
+class TestImage2Array(base_test.BaseTestCase):
+    ''' Tests for image2array
+    
+    '''
+    def __init__(self):
+        self.expected = np.array([[1, 2, 3], [5, 6, 7], [8, 9, 10]])
+
+    def test_actual_tiff(self):
+        # make tif
+        tifffile.imsave('test.tif',self.expected)
+        
+        # load from tif
+        actual   = datahandler.image2array('test.tif')
+        
+        # remove tif
+        os.remove('test.tif')
+        
+        # assert equality
+        self.assert_equal(actual, self.expected)
+        
+    def test_array(self):
+        # load from array
+        actual = datahandler.image2array(self.expected)
+        # assert equality
+        self.assert_equal(actual, self.expected)
+
+class TestRois2Masks(base_test.BaseTestCase):
+    ''' Tests for rois2array
+    '''
+    def __init__(self):
+        self.polys = [np.array([[39., 62.], [60., 45.], [48., 71.]]),
+                         np.array([[72., 107.], [78., 130.], [100., 110.]])]
+        self.expected = roitools..getmasks(self.polys, (176, 156))
+
+    def test_imagej_zip(self):
+        # load zip of rois
+        ROI_loc = 'resources/RoiSet.zip'
+        actual = datahandler.rois2arrays(ROI_loc)
+        
+        # assert equality
+        self.assert_equal(actual, self.expected)
+        
+    def test_arrays(self):
+        # load from array
+        actual = datahandler.rois2masks(self.polys)
+        # assert equality
+        self.assert_equal(actual, self.expected)
+        
+    def test_masks(self):
+        # load from masks
+        actual = datahandler.image2array(self.expected)
+        
+        # assert equality
+        self.assert_equal(actual, self.expected)
