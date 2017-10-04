28af69e6847edd4bce8166e0ca62fc1fbae51f4d
swkeemink <swkeemink@scimail.eu>
2017-10-04 15:18:07 +0100

    TST: updated out-of-date tests

--- fissa/tests/test_neuropil.py
+++ fissa/tests/test_neuropil.py
@@ -4,8 +4,6 @@ Tests for the functions in neuropil.py
 Author: Sander Keemink (swkeemink@scimail.eu)
 Created: 2015-11-06
 '''
-
-import unittest
 import numpy as np
 
 from .base_test import BaseTestCase
@@ -35,14 +33,10 @@ class TestNeuropilFuns(BaseTestCase):
                            'iterations': 1,
                            'max_iterations': 1,
                            'random_state': 892}
-        con_desired_nmf = {'converged': False,
-                           'iterations': 1,
+        con_desired_nmf = {'converged': True,
+                           'iterations': 0,
                            'max_iterations': 1,
-                           'random_state': 'not yet implemented'}
-        con_desired_nmfsklearn = {'converged': 'not yet implemented',
-                                  'iterations': 'not yet implemented',
-                                  'max_iterations': 1,
-                                  'random_state': 'not yet implemented'}
+                           'random_state': 892}
 
         # setup fake data
         data = np.array([np.sin(self.x), np.cos(3 * self.x)]) + 1
@@ -74,28 +68,7 @@ class TestNeuropilFuns(BaseTestCase):
                 self.assert_equal(convergence, con_desired_ica)
             elif method == 'nmf':
                 self.assert_equal(convergence, con_desired_nmf)
-            elif method == 'nmf_sklearn':
-                self.assert_equal(convergence, con_desired_nmfsklearn)
 
-        # test all three methods
+        # test all two methods
         run_method('nmf')
         run_method('ica')
-        run_method('nmf_sklearn')
-
-    def test_subtract_pil(self):
-        '''
-        Tests format and effectiveness of npil.subtract_pil()
-        '''
-        # setup fake data
-        np.random.seed(0)
-        data_main = np.random.rand(self.l)
-        data_cont = np.random.rand(self.l)
-        data_measured = data_main + data_cont
-
-        sig_, a = npil.subtract_pil(data_measured, data_cont)
-        self.assert_equal(a, 0.93112459478303866)
-        self.assert_equal(sig_.shape, data_measured.shape)
-
-    @unittest.expectedFailure
-    def test_subtract_dict(self):
-        raise NotImplementedError()
