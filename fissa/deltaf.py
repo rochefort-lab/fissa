c3e3cd1552d828082f0592d4683b0439df484b90
swkeemink <swkeemink@scimail.eu>
2017-10-04 18:20:01 +0100

    DOC: small docstring update

--- fissa/deltaf.py
+++ fissa/deltaf.py
@@ -12,6 +12,9 @@ import scipy.signal
 def findBaselineF0(rawF, fs, axis=0, keepdims=False):
     """Find the baseline for a fluorescence imaging trace line.
 
+    The baseline, F0, is the 5th-percentile of the 1Hz
+    lowpass filtered signal.
+
     Parameters
     ----------
     rawF : array_like
