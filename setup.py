e2fcdbe3eeea9d59dd614d84a538ee97c98b05d7
Scott Lowe <scott.code.lowe@gmail.com>
2018-10-03 14:14:32 -0300

    MAINT: Remove multiprocessing from requirements, as it is built-in
    It is built-in since Python 2.6, and thus there is no installable
    package for Python 3. Since we don't support Python <2.7, we don't
    need to worry about having it as a dependency.

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ install_requires = ['numpy>=1.13.1',
                     'scikit-image>=0.13.0',
                     'shapely>=1.5.17.post1',
                     'tifffile>=0.12.1',
-                    'multiprocessing>=2.6.2.1',
                     'pillow>=5.0.0',
                     ]
+
 extras_require = {}
 
 # Notebook dependencies for plotting
