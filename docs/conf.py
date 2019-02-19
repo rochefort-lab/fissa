d5e9734fc59c90652d4038d6dca8b1ac718b60fc
Scott Lowe <scott.code.lowe@gmail.com>
2019-02-19 19:05:36 -0400

    DOC: Add intersphinx references (partially working)
    - Numpy is linking correctly (numpy.ndarray does link).
    - Pillow is not linking correctly (i.e. PIL.Image doesn't link).
    - skimage is not linking correctly (skimage.measure doesn't link).

--- docs/conf.py
+++ docs/conf.py
@@ -244,7 +244,15 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'NumPy': ('https://docs.scipy.org/doc/numpy/', None),
+    'SciPy': ('https://docs.scipy.org/doc/scipy/reference', None),
+    'matplotlib': ('https://matplotlib.org', None),
+    'sklearn': ('https://scikit-learn.org/stable', None),
+    'Pillow': ('https://pillow.readthedocs.io/en/stable/', None),
+    'skimage': ('https://github.com/scikit-image/docs/raw/gh-pages/dev/', None),
+}
 
 # -- Options for todo extension ----------------------------------------------
 
