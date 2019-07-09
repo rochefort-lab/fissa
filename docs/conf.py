d0d86761e3d66879039be77e6a08737487947923
Scott Lowe <scott.code.lowe@gmail.com>
2019-07-09 16:39:12 -0300

    MAINT: Ignore PEP8 E402 violation in conf.py
    We need to import __meta__ after using sys.path.insert().

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,8 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
-from fissa import __meta__ as meta
+
+from fissa import __meta__ as meta  # noqa: E402
 
 
 # -- Project information -----------------------------------------------------
