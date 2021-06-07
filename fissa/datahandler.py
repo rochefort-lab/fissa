7a486433bd187be7a2181671e878108da9a1457f
swkeemink <swkeemink@scimail.eu>
2021-06-07 12:06:29 +0200

    RF: added and renamed framebyframe datahandler to basic datahandler file

--- fissa/datahandler.py
+++ fissa/datahandler.py
@@ -210,3 +210,135 @@ class DataHandler(DataHandlerAbstract):
             out[i, :] = data[:, masks[i]].mean(axis=1)
 
         return out
+
+class DataHandlerPillow(DataHandlerAbstract):
+    '''Use Pillow to read TIFF files frame-by-frame. Slower but less memory intensive than the DataHandler() class.'''
+
+    def image2array(self, image):
+        """Open a given image file as a PIL.Image instance.
+
+        Parameters
+        ----------
+        image : str or file
+            A filename (string) of a TIFF image file, a pathlib.Path object,
+            or a file object.
+
+        Returns
+        -------
+        PIL.Image
+            Handle from which frames can be loaded.
+
+        """
+        return Image.open(image)
+
+    def getmean(self, data):
+        """Determine the mean image across all frames.
+
+        Parameters
+        ----------
+        data : PIL.Image
+            An open PIL.Image handle to a multi-frame TIFF image.
+
+        Returns
+        -------
+        numpy.ndarray
+            y-by-x array for the mean values.
+
+        """
+        # We don't load the entire image into memory at once, because
+        # it is likely to be rather large.
+        # Initialise holding array with zeros
+        avg = np.zeros(data.size[::-1])
+
+        # Make sure we seek to the first frame before iterating. This is
+        # because the Iterator outputs the value for the current frame for
+        # `img` first, due to a bug in Pillow<=3.1.
+        data.seek(0)
+
+        # Loop over all frames and sum the pixel intensities together
+        for frame in ImageSequence.Iterator(data):
+            avg += np.asarray(frame)
+
+        # Divide by number of frames to find the average
+        avg /= data.n_frames
+        return avg
+
+    def rois2masks(self, rois, data):
+        """Take the object 'rois' and returns it as a list of binary masks.
+
+        Parameters
+        ----------
+        rois : str or list of array_like
+            Either a string with imagej roi zip location, list of arrays encoding
+            polygons, or list of binary arrays representing masks
+        data : PIL.Image
+            An open PIL.Image handle to a multi-frame TIFF image.
+
+        Returns
+        -------
+        list
+            List of binary arrays (i.e. masks).
+
+        """
+        # get the image shape
+        shape = data.size[::-1]
+
+        # If rois is string, we first need to read the contents of the file
+        if isinstance(rois, basestring):
+            rois = roitools.readrois(rois)
+
+        if not isinstance(rois, collections.Sequence):
+            raise TypeError(
+                'Wrong ROIs input format: expected a list or sequence, but got'
+                ' a {}'.format(rois.__class__)
+            )
+
+        # if it's a something by 2 array (or vice versa), assume polygons
+        if np.shape(rois[0])[1] == 2 or np.shape(rois[0])[0] == 2:
+            return roitools.getmasks(rois, shape)
+        # if it's a list of bigger arrays, assume masks
+        elif np.shape(rois[0]) == shape:
+            return rois
+
+        raise ValueError('Wrong ROIs input format: unfamiliar shape.')
+
+    def extracttraces(self, data, masks):
+        """Get the traces for each mask in masks from data.
+
+        Parameters
+        ----------
+        data : PIL.Image
+            An open PIL.Image handle to a multi-frame TIFF image.
+        masks : list of array_like
+            List of binary arrays.
+
+        Returns
+        -------
+        numpy.ndarray
+            Trace for each mask. Shaped `(len(masks), n_frames)`.
+
+        """
+        # get the number rois
+        nrois = len(masks)
+
+        # get number of frames, and start at zeros
+        data.seek(0)
+        nframes = data.n_frames
+
+        # predefine array with the data
+        out = np.zeros((nrois, nframes))
+
+        # for each frame, get the data
+        for f in range(nframes):
+            # set frame
+            data.seek(f)
+
+            # make numpy array
+            curframe = np.asarray(data)
+
+            # loop over masks
+            for i in range(nrois):
+                # get mean data from mask
+                out[i, f] = np.mean(curframe[masks[i]])
+
+        return out
