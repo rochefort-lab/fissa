3059d9bef03031d7b180d57e4f74502941165212
Scott Lowe <scott.code.lowe@gmail.com>
2021-06-08 17:13:28 +0100

    DOC: Improve docstrings

--- fissa/extraction.py
+++ fissa/extraction.py
@@ -1,13 +1,9 @@
-"""FISSA functions to handle image and roi objects and return the right format.
-
-If a custom version of this file is used (which can be defined at the
-declaration of the core FISSA Experiment class), it should have the same
-functions as here, with the same inputs and outputs.
+"""
+DataHandler classes to handle and manipulate image and ROI objects.
 
 Authors:
     - Sander W Keemink <swkeemink@scimail.eu>
     - Scott C Lowe <scott.code.lowe@gmail.com>
-
 """
 
 from past.builtins import basestring
@@ -23,7 +19,8 @@ from . import roitools
 
 
 class DataHandlerAbstract():
-    """Abstract class for a data handler.
+    """
+    Abstract class for a data handler.
 
     Note
     ----
@@ -42,80 +39,87 @@ class DataHandlerAbstract():
         pass
 
     def image2array(self, image):
-        """Loads an image as an array.
+        """
+        Load data (from a path) as an array, or similar internal structure.
 
         Parameters
         ----------
         image : type
-            Some string/object class/etc.
+            Some handle to, or representation of, the raw imagery data.
 
         Returns
         -------
-        type
-            Whatever format you want the data to be saved in, and used by the other functions.
+        data : type
+            Internal representation of the images which will be used by all
+            the other methods in this class.
         """
         raise NotImplementedError()
 
     def getmean(self, data):
-        """Determine the mean image across all frames.
+        """
+        Determine the mean image across all frames.
 
         Must return a 2D :class:`numpy.ndarray`.
 
         Parameters
         ----------
         data : type
-            Whatever format is returned by :meth:`image2array`.
+            The same object as returned by :meth:`image2array`.
 
         Returns
         -------
-        numpy.ndarray
-            y by x array for the mean values
-
+        mean : numpy.ndarray
+            Mean image as a 2D, y-by-x, array.
         """
         raise NotImplementedError()
 
     def rois2masks(self, rois, data):
-        """Take the object `rois` and returns it as a list of binary masks.
+        """
+        Convert `rois` into a collection of binary masks.
 
         Parameters
         ----------
         rois : type
-            However the ROIs are formatted before becoming binary masks.
+            Incoming ROIs are formatted before becoming binary masks.
         data : type
-            The same format as is returned by :meth:`image2array`.
+            The same object as returned by :meth:`image2array`.
 
         Returns
         -------
-        type
-            Whatever format is needed in :meth:`extracttraces`.
+        masks : type
+            Masks, in a format accepted by :meth:`extracttraces`.
         """
         raise NotImplementedError()
 
     def extracttraces(self, data, masks):
-        """Extracts a temporal trace for each spatial mask.
+        """
+        Extract from data the average signal within each mask, across time.
 
-       Must return a 2D :class:`numpy.ndarray`.
+        Must return a 2D :class:`numpy.ndarray`.
 
         Parameters
         ----------
         data : type
-            Whatever format is returned by :meth:`image2array`.
+            The same object as returned by :meth:`image2array`.
         masks : type
-            Whatever format is returned by :meth:`rois2masks`.
+            The same object as returned by :meth:`rois2masks`.
 
         Returns
         -------
-        numpy.ndarray
+        traces : numpy.ndarray
             Trace for each mask, shaped ``(len(masks), n_frames)``.
-
         """
         raise NotImplementedError()
 
+
 class DataHandlerTifffile(DataHandlerAbstract):
-    """Using tifffile to interact with TIFF images."""
+    """
+    Extract data from TIFF images using tifffile.
+    """
 
     def image2array(self, image):
-        """Loads a TIFF image from disk.
+        """
+        Load a TIFF image from disk.
 
         Parameters
         ----------
@@ -126,26 +130,25 @@ class DataHandlerTifffile(DataHandlerAbstract):
         -------
         numpy.ndarray
             A 3D array containing the data, with dimensions corresponding to
-            `(frames, y_coordinate, x_coordinate)`.
-
+            ``(frames, y_coordinate, x_coordinate)``.
         """
         if isinstance(image, basestring):
             return tifffile.imread(image)
         return np.array(image)
 
     def getmean(self, data):
-        """Determine the mean image across all frames.
+        """
+        Determine the mean image across all frames.
 
         Parameters
         ----------
         data : array_like
-            Data array as made by :meth:`image2array`. Should be shaped `(frames, y, x)`.
+            Data array as made by :meth:`image2array`, shaped ``(frames, y, x)``.
 
         Returns
         -------
         numpy.ndarray
             y by x array for the mean values
-
         """
         return data.mean(axis=0)
 
@@ -154,17 +157,18 @@ class DataHandlerTifffile(DataHandlerAbstract):
 
         Parameters
         ----------
-        rois : string or list of array_like
-            Either a string with imagej roi zip location, list of arrays encoding
-            polygons, or list of binary arrays representing masks
+        rois : str or list of array_like
+            Either a string containing a path to an ImageJ roi zip file,
+            or a list of arrays encoding polygons, or list of binary arrays
+            representing masks.
         data : array
-            Data array as made by :meth:`image2array`. Must be shaped `(frames, y, x)`.
+            Data array as made by :meth:`image2array`. Must be shaped
+            ``(frames, y, x)``.
 
         Returns
         -------
-        list
-            List of binary arrays (i.e. masks)
-
+        masks : list of numpy.ndarray
+            List of binary arrays.
         """
         # get the image shape
         shape = data.shape[1:]
@@ -189,21 +193,20 @@ class DataHandlerTifffile(DataHandlerAbstract):
         raise ValueError('Wrong ROIs input format: unfamiliar shape.')
 
     def extracttraces(self, data, masks):
-        """Extracts a temporal trace for each spatial mask.
+        """
+        Extract a temporal trace for each spatial mask.
 
         Parameters
         ----------
         data : array_like
-            Data array as made by :meth:`image2array`. Should be shaped
-            `(frames, y, x)`.
+            Data array as made by :meth:`image2array`, shaped ``(frames, y, x)``.
         masks : list of array_like
             List of binary arrays.
 
         Returns
         -------
-        numpy.ndarray
-            Trace for each mask. Shaped `(len(masks), n_frames)`.
-
+        traces : numpy.ndarray
+            Trace for each mask, shaped ``(len(masks), n_frames)``.
         """
         # get the number rois and frames
         nrois = len(masks)
@@ -219,39 +222,44 @@ class DataHandlerTifffile(DataHandlerAbstract):
 
         return out
 
+
 class DataHandlerPillow(DataHandlerAbstract):
-    '''Use Pillow to read TIFF files frame-by-frame. Slower but less memory intensive than the DataHandler() class.'''
+    """
+    Extract data from TIFF images frame-by-frame using Pillow (:class:`PIL.Image`).
+
+    Slower, but less memory-intensive than :class:`DataHandlerTifffile`.
+    """
 
     def image2array(self, image):
-        """Open a given image file as a PIL.Image instance.
+        """
+        Open an image file as a :class:`PIL.Image` instance.
 
         Parameters
         ----------
         image : str or file
-            A filename (string) of a TIFF image file, a pathlib.Path object,
-            or a file object.
+            A filename (string) of a TIFF image file, a :class:`pathlib.Path`
+            object, or a file object.
 
         Returns
         -------
-        PIL.Image
+        data : PIL.Image
             Handle from which frames can be loaded.
-
         """
         return Image.open(image)
 
     def getmean(self, data):
-        """Determine the mean image across all frames.
+        """
+        Determine the mean image across all frames.
 
         Parameters
         ----------
         data : PIL.Image
-            An open PIL.Image handle to a multi-frame TIFF image.
+            An open :class:`PIL.Image` handle to a multi-frame TIFF image.
 
         Returns
         -------
-        numpy.ndarray
+        mean : numpy.ndarray
             y-by-x array for the mean values.
-
         """
         # We don't load the entire image into memory at once, because
         # it is likely to be rather large.
@@ -272,21 +280,22 @@ class DataHandlerPillow(DataHandlerAbstract):
         return avg
 
     def rois2masks(self, rois, data):
-        """Take the object 'rois' and returns it as a list of binary masks.
+        """
+        Take the object `rois` and returns it as a list of binary masks.
 
         Parameters
         ----------
         rois : str or list of array_like
-            Either a string with imagej roi zip location, list of arrays encoding
-            polygons, or list of binary arrays representing masks
+            Either a string containing a path to an ImageJ roi zip file,
+            or a list of arrays encoding polygons, or list of binary arrays
+            representing masks.
         data : PIL.Image
-            An open PIL.Image handle to a multi-frame TIFF image.
+            An open :class:`PIL.Image` handle to a multi-frame TIFF image.
 
         Returns
         -------
-        list
-            List of binary arrays (i.e. masks).
-
+        masks : list of numpy.ndarray
+            List of binary arrays.
         """
         # get the image shape
         shape = data.size[::-1]
@@ -311,20 +320,20 @@ class DataHandlerPillow(DataHandlerAbstract):
         raise ValueError('Wrong ROIs input format: unfamiliar shape.')
 
     def extracttraces(self, data, masks):
-        """Get the traces for each mask in masks from data.
+        """
+        Extract the average signal within each mask across the data.
 
         Parameters
         ----------
         data : PIL.Image
-            An open PIL.Image handle to a multi-frame TIFF image.
+            An open :class:`PIL.Image` handle to a multi-frame TIFF image.
         masks : list of array_like
             List of binary arrays.
 
         Returns
         -------
-        numpy.ndarray
-            Trace for each mask. Shaped `(len(masks), n_frames)`.
-
+        traces : numpy.ndarray
+            Trace for each mask, shaped ``(len(masks), n_frames)``.
         """
         # get the number rois
         nrois = len(masks)
