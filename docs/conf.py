87b65ebb43dc8d762ffdacbc6d1d2a44981d81fa
Scott Lowe <scott.code.lowe@gmail.com>
2021-06-12 12:20:54 +0100

    DOC: Add array_like term to napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -115,8 +115,12 @@ napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False
 napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
-napoleon_use_param = True
 napoleon_use_rtype = True
+napoleon_use_param = True
+napoleon_type_aliases = {
+    "array_like": ":term:`array_like`",
+    "array-like": ":term:`array-like <array_like>`",
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
