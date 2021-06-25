b95b32a6b80b52ad67f4445799e4951e0a3f230c
Scott Lowe <scott.code.lowe@gmail.com>
2021-06-25 13:26:02 +0100

    TST:RF: Use a Mixin class for neuropil tests

--- fissa/tests/test_neuropil.py
+++ fissa/tests/test_neuropil.py
@@ -9,13 +9,14 @@ from .base_test import BaseTestCase
 from .. import neuropil as npil
 
 
-class TestNeuropilFuns(BaseTestCase):
+class NeuropilMixin:
     """Neuropil functions testing class."""
 
-    def setup_class(self):
+    def setUp(self):
         """Set up the basic variables."""
         # length of arrays in tests
         self.l = 100
+        self.shape_desired = (2, self.l)
         # setup basic x values for fake data
         self.x = np.linspace(0, np.pi * 2, self.l)
         # Generate simple source data
@@ -24,46 +25,61 @@ class TestNeuropilFuns(BaseTestCase):
         self.W = np.array([[0.2, 0.8], [0.8, 0.2]])
         self.S = np.dot(self.W, self.data)
 
-    def test_separate(self):
-        """Tests the separate function data format."""
-        # desired shapes
-        shape_desired = (2, self.l)
+    def run_method(self, method, expected_converged=None, **kwargs):
+        """Test a single method, with specific parameters."""
+        # Run the separation routine
+        S_sep, S_matched, A_sep, convergence = npil.separate(
+            self.S, sep_method=method, **kwargs
+        )
+        # Ensure output shapes are as expected
+        self.assert_equal(S_sep.shape, self.shape_desired)
+        self.assert_equal(S_matched.shape, self.shape_desired)
+        self.assert_equal(S_sep.shape, self.shape_desired)
+        # If specified, assert that the result is as expected
+        if expected_converged is not None:
+            self.assert_equal(convergence['converged'], expected_converged)
 
-        # function for testing a method
-        def run_method(method, expected_converged=None, **kwargs):
-            """Test a single method, with specific parameters."""
-            # Run the separation routine
-            S_sep, S_matched, A_sep, convergence = npil.separate(
-                self.S, sep_method=method, **kwargs
-            )
-            # Ensure output shapes are as expected
-            self.assert_equal(S_sep.shape, shape_desired)
-            self.assert_equal(S_matched.shape, shape_desired)
-            self.assert_equal(S_sep.shape, shape_desired)
-            # If specified, assert that the result is as expected
-            if expected_converged is not None:
-                self.assert_equal(convergence['converged'], expected_converged)
+    def test_method(self):
+        self.run_method(self.method, expected_converged=True, maxtries=1)
 
-        # Run tests
-        i_subtest = 0
-        for method in ["nmf", "ica", "FactorAnalysis"]:
-            with self.subTest(i_subtest):
-                run_method(method, expected_converged=True, maxtries=1, n=2)
-            i_subtest += 1
-            with self.subTest(i_subtest):
-                run_method(method, expected_converged=True, maxtries=1)
-            i_subtest += 1
-            with self.subTest(i_subtest):
-                run_method(method, expected_converged=True, maxtries=1, random_state=0)
-            i_subtest += 1
-            with self.subTest(i_subtest):
-                run_method(method, maxiter=1, maxtries=3)
-            i_subtest += 1
+    def test_reduce_dim(self):
+        self.run_method(self.method, expected_converged=True, maxtries=1, n=2)
 
-        with self.subTest(i_subtest):
-            run_method('nmf', expected_converged=True, alpha=.2)
-        i_subtest += 1
+    def test_manual_seed(self):
+        self.run_method(
+            self.method,
+            expected_converged=True,
+            maxtries=1,
+            random_state=0,
+        )
+
+    def test_retry(self):
+        self.run_method(self.method, maxiter=1, maxtries=3)
+
+
+class TestNeuropilNMF(BaseTestCase, NeuropilMixin):
+
+    def setUp(self):
+        NeuropilMixin.setUp(self)
+        self.method = "nmf"
+
+    def test_nmf_manual_alpha(self):
+        self.run_method(self.method, expected_converged=True, alpha=.2)
 
     def test_badmethod(self):
         with self.assertRaises(ValueError):
             npil.separate(self.S, sep_method='bogus_method')
+
+
+class TestNeuropilICA(BaseTestCase, NeuropilMixin):
+
+    def setUp(self):
+        NeuropilMixin.setUp(self)
+        self.method = "ica"
+
+
+class TestNeuropilFA(BaseTestCase, NeuropilMixin):
+
+    def setUp(self):
+        NeuropilMixin.setUp(self)
+        self.method = "FactorAnalysis"
