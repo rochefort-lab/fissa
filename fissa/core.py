c69d4618075b21b2d5f371c97cf4ab752bf58535
Scott Lowe <scott.code.lowe@gmail.com>
2021-07-09 16:43:12 +0100

    MNT: Improve output when rois do not all converge

--- fissa/core.py
+++ fissa/core.py
@@ -975,9 +975,11 @@ class Experiment:
             print("Finished separating all the ROI signals.")
             if len(non_converged_cells) > 0:
                 print(
-                    "The following ROI numbers did not fully converge: {}."
-                    " Consider changing FISSA parameters if this happens often and/or"
-                    " to a lot of cells.".format(non_converged_cells)
+                    "The following {} ROIs did not fully converge: {}."
+                    " Consider changing FISSA parameters if this happens often"
+                    " and/or to a lot of cells.".format(
+                        len(non_converged_cells), non_converged_cells
+                    )
                 )
 
         # Set outputs
