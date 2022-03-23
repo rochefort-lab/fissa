827660ce7288a2802f9bbc9b6473eb81f34e9b64
Scott Lowe <scott.code.lowe@gmail.com>
2022-03-23 18:02:42 +0000

    DOC: Install pandoc when building docs

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,26 @@ release = meta.version
 version = ".".join(release.split(".")[0:2])
 
 
+# -- Install pandoc ----------------------------------------------------------
+
+
+def ensure_pandoc_installed(_):
+    import pypandoc
+
+    # Download pandoc if necessary. If pandoc is already installed and on
+    # the PATH, the installed version will be used. Otherwise, we will
+    # download a copy of pandoc into docs/bin/ and add that to our PATH.
+    pandoc_dir = os.path.join(DOCS_DIRECTORY, "bin")
+    # Add
+    if pandoc_dir not in os.environ["PATH"].split(os.pathsep):
+        os.environ["PATH"] += os.pathsep + pandoc_dir
+    pypandoc.ensure_pandoc_installed(
+        quiet=True,
+        targetfolder=pandoc_dir,
+        delete_installer=True,
+    )
+
+
 # -- Automatically generate API documentation --------------------------------
 
 
@@ -83,6 +103,7 @@ def retitle_modules(_):
 
 
 def setup(app):
+    app.connect("builder-inited", ensure_pandoc_installed)
     app.connect("builder-inited", run_apidoc)
     app.connect("builder-inited", retitle_modules)
 
