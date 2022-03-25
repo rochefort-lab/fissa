83627112ee034da57be62d0cfd2511152aa90c31
Scott Lowe <scott.code.lowe@gmail.com>
2022-03-25 12:10:31 +0000

    REL: Version 1.0.0

--- fissa/__meta__.py
+++ fissa/__meta__.py
@@ -1,6 +1,6 @@
 name = "fissa"
 path = name
-version = "1.0.dev0"
+version = "1.0.0"
 author = "Sander Keemink and Scott Lowe"
 author_email = "swkeemink@scimail.eu"
 description = "A Python Library estimating somatic signals in 2-photon data"
